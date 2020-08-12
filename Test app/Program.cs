f5f7a93a80ce9ccb45a4beecaa7210b5fd51e916
Gouthaman Mathavi <gouthaman@base2software.co.nz>
2020-08-12 18:08:38 +1200

    Add project files.

---
+++ Test app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, This is my test app. I am going to start a new role at Base2 Software and im prettu much excited");
+        }
+    }
+}
