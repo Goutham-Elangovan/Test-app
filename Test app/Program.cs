3da503087e04b95a71ecde016ef57e24605bde1f
Gouthaman Mathavi <gouthaman@base2software.co.nz>
2020-08-12 18:49:31 +1200

    repeat

--- Test app/Program.cs
+++ Test app/Program.cs
@@ -2,17 +2,16 @@
 
 namespace Test_app
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello, This is my test app. " +
                 "I am going to start a new role at Base2 Software and im prettu much excited");
-            
+
             Console.WriteLine("Adding a new commit");
 
             Console.WriteLine("2nd commit");
-
         }
     }
 }
