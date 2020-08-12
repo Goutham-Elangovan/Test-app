858684017c2bc650b4750bc1d91ddaa0cd02a5af
Gouthaman Mathavi <gouthaman@base2software.co.nz>
2020-08-12 18:15:26 +1200

    first commit from the test app

--- Test app/Program.cs
+++ Test app/Program.cs
@@ -6,7 +6,11 @@ namespace Test_app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, This is my test app. I am going to start a new role at Base2 Software and im prettu much excited");
+            Console.WriteLine("Hello, This is my test app. " +
+                "I am going to start a new role at Base2 Software and im prettu much excited");
+            
+            Console.WriteLine("Adding a new commit");
+
         }
     }
 }
